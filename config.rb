248a49b1eb367719a5e8481cf6e71d2bbc1dc790
sorata1226 <cita16045@g.nihon-u.ac.jp>
2018-12-05 15:29:19 +0900

    add

--- config.rb
+++ config.rb
@@ -4,5 +4,5 @@
 @password='taku1226' #gmailのパスワード
 @mail_label = 'Work/maid-tyan' #メイドちゃん宛のメールを事前に振り分けてください。
 
-@address ="taku31226orz@softbank.ne.jp" #送信先アドレス
+@address ="taku31226orz@softbank.ne.jp"  #送信先アドレス
 @zipcode = "000-0000" #郵便番号
